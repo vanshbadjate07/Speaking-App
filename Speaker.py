fbe91ff1ecbedab0cedbb84c52fe6642dfe76ade
Vansh Badjate <128991304+vanshbadjate07@users.noreply.github.com>
2024-09-23 11:07:23 +0530

    Update Speaker.py

--- Speaker.py
+++ Speaker.py
@@ -1,9 +1,20 @@
 import os
+import platform
+import win32com.client as wincom
+
 print("*Welcome to Vansh Speaking App*")
 
 while True:
+    os_type = platform.system()
     s = input("Enter what you want to speak: ")
     if s.lower() == "exit":
         break
-    text = f"say {s}"
-    os.system(text)
+    
+    if os_type == "Darwin":
+        text = f"say {s}"
+        os.system(text)
+    elif os_type == "Windows":
+        speak = wincom.Dispatch("SAPI.SpVoice")
+        speak.Speak(s)  # Use 's' instead of 'text'
+    else:
+        print("Use this code on either Mac or Windows")
